7f777767f165fffe9297f5f309710af54adcbde4
houstondapaz <contact@houstondapaz.dev>
2024-12-20 14:30:40 -0300

    fix: add update create by collumns

--- src/shared/entities/audit.entity.ts
+++ src/shared/entities/audit.entity.ts
@@ -3,14 +3,16 @@ import {
   DeleteDateColumn,
   UpdateDateColumn,
   PrimaryGeneratedColumn,
+  Column,
 } from 'typeorm';
-
 export class AuditEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+  @Column()
   createdById: string;
   @CreateDateColumn()
   createdAt: Date;
+  @Column()
   updatedById: string;
   @UpdateDateColumn()
   updatedAt: Date;
